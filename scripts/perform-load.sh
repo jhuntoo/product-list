002ffab542504339927e0e60de4520eb68ff595c
Jonathon Lee <jonathan.y.lee@johnlewis.co.uk>
2020-12-11 09:11:02 +0000

    updated container CPU limits to trigger autoscaling sooner

--- scripts/perform-load.sh
+++ scripts/perform-load.sh
@@ -1,4 +1,4 @@
 set -euox pipefail
 
 docker run --rm -it peterevans/vegeta sh -c \
-  "echo 'GET http://${LOADBALANCER_IP}' | vegeta attack -rate=5 -duration=5m | vegeta encode"
+  "echo 'GET http://${LOADBALANCER_IP}' | vegeta attack -rate=10 -duration=5m | vegeta encode"
